81188c6d9448cb64c0205d1127da4797d3fc5d2d
devsdmf <devsdmf@gmail.com>
2015-04-01 13:31:56 -0300

    Removed debug lines and added process forking

--- streammer.c
+++ streammer.c
@@ -1,60 +1,47 @@
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <mikmod.h>
+#include <stdlib.h>
 
 int main() 
 {
-    MODULE *module;
+    pid_t pID = fork();
 
-    MikMod_InitThreads();
-    
-    MikMod_RegisterAllDrivers();
-    MikMod_RegisterAllLoaders();
+    if (pID == 0) {
+        MODULE *module;
 
-    md_mode |= DMODE_SOFT_MUSIC | DMODE_NOISEREDUCTION | DMODE_INTERP;
-    if (MikMod_Init("")) {
-        fprintf(stderr, "Could not initialize sound, reason: %s\n",MikMod_strerror(MikMod_errno));
-        return 1;
-    }
+        MikMod_InitThreads();
+        
+        MikMod_RegisterAllDrivers();
+        MikMod_RegisterAllLoaders();
 
-    module = Player_Load("audio-embrace-1.xm", 128, 1);
-    if (module) {
-        module->wrap = 1;
-        module->loop = 0;
-
-        printf("Playing %s\n", module->songname);
-        printf("Channels: %d\n",module->numchn);
-        printf("Voices: %d\n",module->numvoices);
-        printf("Patterns: %d\n",module->numpat);
-        printf("Samples: %d\n",module->numsmp);
-        printf("Time: %d\n",module->sngtime);
-        printf("Wrap: %d\n",module->wrap);
-        printf("Loop: %d\n",module->loop);
-        printf("\n");
-
-        for (int i=0;i<=module->numsmp;i++) {
-            SAMPLE sample;
-            sample = module->samples[i];
-            printf("----> SAMPLE\n");
-            printf("----> Length: %d\n",sample.length);
-            printf("----> Loop start: %d\n",sample.loopstart);
-            printf("----> Loop end: %d\n",sample.loopend);
-            printf("\n");
+        md_mode |= DMODE_SOFT_MUSIC | DMODE_NOISEREDUCTION | DMODE_INTERP;
+        if (MikMod_Init("")) {
+            fprintf(stderr, "Could not initialize sound, reason: %s\n",MikMod_strerror(MikMod_errno));
+            return 1;
         }
 
-        Player_Start(module);
+        module = Player_Load("audio-embrace-1.xm", 64, 1);
+        if (module) {
+            module->wrap = 1;
+            module->loop = 0;
+
+            printf("Child process: %d\n",getpid());  
+            Player_Start(module);
  
-        while (Player_Active()) {
-            usleep(100000);
-            MikMod_Update();
-        }
+            while (Player_Active()) {
+                usleep(10000);
+                MikMod_Update();
+            }
+        } else
+            fprintf(stderr, "Could not load module, reason: %s\n", MikMod_strerror(MikMod_errno));
 
-        Player_Stop();
-        Player_Free(module);
-    } else
-        fprintf(stderr, "Could not load module, reason: %s\n", MikMod_strerror(MikMod_errno));
-
-    MikMod_Exit();
+        MikMod_Exit();
+    } else if (pID < 0) {
+        fprintf(stderr, "Failed to fork\n");
+        exit(1);
+    }
 
     return 0;
 }
